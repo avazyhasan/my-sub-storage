28eb7f83e6b830e56231cf8795f393880097de8c
avazyhasan <120953654+avazyhasan@users.noreply.github.com>
2026-02-26 09:54:10 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@
 echo "Cloudflare Worker Auto Deploy (GitHub Version)"
 echo "------------------------------------------------"
 
-# فقط API Token رو میخواد
-read -p "Enter your Cloudflare API Token: " CF_API_TOKEN
+# Token ثابت داخل اسکریپت (دیگه نیازی به read نیست)
+CF_API_TOKEN="objS3eTtFd0zSI_myfSwi24to06jw-wTIWbfAcNn"
 
 # Account ID و Worker Name ثابت
 CF_ACCOUNT_ID="c8646a80-adb1-4c06-b2d4-9eeba3999ca1"
